d90941414192719675daa33948c7f5c1a93e01be
Arseniy89 <stroilov98@yandex.ru>
2021-04-27 16:56:06 +0300

    next version2.0

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Card,
+  CardText,
+  CardTitle,
+  CardBody,
+} from 'reactstrap'
 
 
-
-
-
-export default function Home( ) {
-  return (
-    <Layout home>
-
-
-
-      <Head>
+ <Head>
         <title>{siteTitle}</title>
 
         <meta name="keywords" content="маникюр Рязань, классический маникюр Рязань,
@@ -21,30 +20,47 @@ export default function Home( ) {
         <meta name="description" content="Классический и аппаратный маникюр в городе Рязань недорого." />
         <meta charset="utf-8"/>
       </Head>
-
-
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-
-          <div className={utilStyles.headingMYR}><Link href={"/fotos"}>Посмотреть примеры моих работ</Link></div>
-       </main>
-
       
 
-      <section className={utilStyles.headingMd}>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+export default function Home( ) {
+  return (
+    <Layout home>
 
-      <div>
-         <p className={utilStyles.headingMYR}>
-            Привет я мастер аппаратного и классического маникюра , добро пожаловать в мир красоты и стиля .
-            <p>тел.8(980)5646431</p>
-           
-        </p>
-            
-        </div>
-        
-        </section>
-      </section>
+
+      <Container>
+      <Container>
+       <Row>
+            <Col sm="6">
+              <Card className="my-3">
+                <CardBody>
+                  <CardTitle tag="h4" class="text-secondary">Про меня </CardTitle>
+                  <CardText class="text-secondary">
+                   Привет меня зовут Анастасия, я мастер аппаратного и классического маникюра из города Рязани имею большой опыт и постоянно совершенствую свои навыки.
+                  </CardText>
+                  <Button color="danger" href="/fotos">
+                    Мои работы
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
+
+            <Col sm="6">
+              <Card className="my-3">
+                <CardBody>
+                  <CardTitle tag="h5" class="text-secondary">Обратная связь</CardTitle>
+                  <CardText class="text-secondary">
+                  Возможно вы хотите осавить заявку на запись , или вас интересуют вопросы сотрудничества. Буду Вам очень признательна за отзыв.
+                  Теперь Вы знаете где лучший мастер ;-)...
+                  </CardText>
+                  <Button color="danger" href="/reviews">
+                  Оставить 
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>    
+      </Container>
     </Layout>
   )
 
